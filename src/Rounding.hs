62f5fe9d19c0a96fd6c7df7407b6893d8dacea51
Heath Weiss <HeathWeiss@hotmail.com>
2020-03-10 14:06:28 -0600

    backup

---
+++ src/Rounding.hs
@@ -0,0 +1,8 @@
+{-
+Try out the Rounding library. https://www.stackage.org/package/rounded
+
+Can't get it to install. Intstalled mpfr but stack is not finding it. Look in ~\mpfr\version.c which when run, shows that mpfr ins installed, but perhaps on in a standard location.
+-}
+
+module Rounding() where
+
