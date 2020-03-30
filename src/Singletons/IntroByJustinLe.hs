387a87ebb0b8d3ae213d487f0ff6e2e1d923ccd9
Heath Weiss <HeathWeiss@hotmail.com>
2020-03-30 12:04:40 -0600

    initial commit

---
+++ src/Singletons/IntroByJustinLe.hs
@@ -0,0 +1,8 @@
+
+{-
+Based on: https://blog.jle.im/entry/introduction-to-singletons-1.html
+He has blog topics on many Haskell topics.
+Has written a mutable library: https://mutable.jle.im/
+-}
+module Singletons.IntroByJustinLe() where
+
